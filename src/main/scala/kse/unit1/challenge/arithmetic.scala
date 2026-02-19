ecff038478373c2169f2828583d26251b8b5abe6
akorniienko04 <akorniienko@kse.org.ua>
2026-02-19 21:28:26 +0200

    Update arithmetic.scala

--- src/main/scala/kse/unit1/challenge/arithmetic.scala
+++ src/main/scala/kse/unit1/challenge/arithmetic.scala
@@ -6,20 +6,44 @@ object arithmetic:
 
   type Number = Long
 
-  val Z: Number => Number = ???
-  val S: Number => Number = ???
+  val Z: Number => Number = value => if value == 0 then 1 else 0
+  val S: Number => Number = value => value + 1
 
   /**
    * Optional task: make `addition` tail-recursive.
    */
-  val addition: (Number, Number) => Number = ???
+  val addition: (Number, Number) => Number =
+    (l, r) =>
+
+      @tailrec
+      def loop(left: Number, right: Number): Number =
+        if right == 0 then left
+        else loop(S(left), right - 1)
+
+      loop(l, r)
 
   /**
    * Optional task: make `multiplication` tail-recursive.
    */
-  val multiplication: (Number, Number) => Number = ???
+  val multiplication: (Number, Number) => Number =
+    (l, r) =>
+
+      @tailrec
+      def loop(acc: Number, times: Number): Number =
+        if times == 0 then acc
+        else loop(addition(acc, l), times - 1)
+
+      loop(0, r)
 
   /**
    * Optional task: make `power` tail-recursive.
    */
-  val power: (Number, Number) => Number = ???
+  val power: (Number, Number) => Number =
+    (osnova, p) =>
+
+      @tailrec
+      def loop(res: Number, exp: Number): Number =
+        if exp == 0 then res
+        else loop(multiplication(res, osnova), exp - 1)
+
+      loop(1, p)
