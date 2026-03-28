75cd93ca4c10543998c7324faeb53fbe6f14249f
akorniienko04 <akorniienko@kse.org.ua>
2026-03-28 22:11:23 +0200

    unit-4

--- src/main/scala/kse/unit4/challenge/numerals.scala
+++ src/main/scala/kse/unit4/challenge/numerals.scala
@@ -10,19 +10,19 @@ object numerals:
 
     def predecessor: Numeral
 
-    def successor: Numeral = ???
+    def successor: Numeral = new Successor(this)
 
     @targetName("greater than")
     infix def >(that: Numeral): Boolean
 
     @targetName("greater or equal to")
-    infix def >=(that: Numeral): Boolean = ???
+    infix def >=(that: Numeral): Boolean = (this > that) || (this == that)
 
     @targetName("less than")
-    infix def <(that: Numeral): Boolean = ???
+    infix def <(that: Numeral): Boolean = !(this >= that)
 
     @targetName("less or equal to")
-    infix def <=(that: Numeral): Boolean = ???
+    infix def <=(that: Numeral): Boolean = !(this > that)
 
     @targetName("addition")
     infix def +(that: Numeral): Numeral
@@ -37,43 +37,51 @@ object numerals:
 
   object Zero extends Numeral:
 
-    def isZero: Boolean = ???
+    def isZero: Boolean = true
 
-    def predecessor: Numeral = ???
+    def predecessor: Numeral = this
 
     @targetName("greater than")
-    infix def >(that: Numeral): Boolean = ???
+    infix def >(that: Numeral): Boolean = false
 
     @targetName("addition")
-    infix def +(that: Numeral): Numeral = ???
+    infix def +(that: Numeral): Numeral = that
 
     // Optional challenge.
     @targetName("subtraction")
-    infix def -(that: Numeral): Numeral = ???
+    infix def -(that: Numeral): Numeral = this
 
-    override def toString: String = ???
+    override def toString: String = "zero"
 
-    override def equals(obj: Any): Boolean = ???
+    override def equals(obj: Any): Boolean = obj match
+      case _: Zero.type => true
+      case _            => false
 
   object Successor:
     def unapply(successor: Successor): Option[Numeral] = Option(successor.predecessor)
 
   class Successor(n: Numeral) extends Numeral:
 
-    def isZero: Boolean = ???
+    def isZero: Boolean = false
 
-    def predecessor: Numeral = ???
+    def predecessor: Numeral = n
 
     @targetName("greater than")
-    infix def >(that: Numeral): Boolean = ???
+    infix def >(that: Numeral): Boolean = that match
+      case Zero         => true
+      case Successor(m) => this.predecessor > m
 
     @targetName("addition")
-    infix def +(that: Numeral): Numeral = ???
+    infix def +(that: Numeral): Numeral = new Successor(n + that)
 
     // Optional challenge.
     @targetName("subtraction")
-    infix def -(that: Numeral): Numeral = ???
+    infix def -(that: Numeral): Numeral = that match
+      case Zero         => this
+      case Successor(m) => this.predecessor - m
 
-    override def toString: String = ???
+    override def toString: String = s"Nat($predecessor)"
 
-    override def equals(obj: Any): Boolean = ???
+    override def equals(obj: Any): Boolean = obj match
+      case s: Successor => this.predecessor == s.predecessor
+      case _            => false
