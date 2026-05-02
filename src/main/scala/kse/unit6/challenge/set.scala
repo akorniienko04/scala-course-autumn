a9814effe7b40f0ed5845e7d340c1cdac232e570
akorniienko04 <akorniienko@kse.org.ua>
2026-05-02 22:50:04 +0300

    complete unit 6

--- src/main/scala/kse/unit6/challenge/set.scala
+++ src/main/scala/kse/unit6/challenge/set.scala
@@ -38,22 +38,22 @@ object set:
 
   case object Empty extends Set[Nothing]:
 
-    infix def forAll(predicate: Nothing => Boolean): Boolean = ???
+    infix def forAll(predicate: Nothing => Boolean): Boolean = true
 
-    infix def exists(predicate: Nothing => Boolean): Boolean = ???
+    infix def exists(predicate: Nothing => Boolean): Boolean = false
 
-    infix def contains[B: Order](x: B): Boolean = ???
+    infix def contains[B: Order](x: B): Boolean = false
 
-    infix def include[B: Order](x: B): Set[B] = ???
+    infix def include[B: Order](x: B): Set[B] = NonEmpty(Empty, x, Empty)
 
     // Optional from the Unit 5. If you haven't implemented it in Unit 5 then skip it
     //    infix def remove[B: Order](x: B): Set[B] = ???
 
     @targetName("union")
-    infix def ∪[B: Order](that: Set[B]): Set[B] = ???
+    infix def ∪[B: Order](that: Set[B]): Set[B] = that
 
     @targetName("intersection")
-    infix def ∩[B: Order](that: Set[B]): Set[B] = ???
+    infix def ∩[B: Order](that: Set[B]): Set[B] = Empty
 
     // Optional from the Unit 5. If you haven't implemented it in Unit 5 then skip it
     //    @targetName("difference")
@@ -61,28 +61,41 @@ object set:
 
     override def toString: String = "[*]"
 
-    override def equals(obj: Any): Boolean = ???
+    override def equals(obj: Any): Boolean = obj.isInstanceOf[Empty]
 
   end Empty
 
   case class NonEmpty[A](left: Set[A], element: A, right: Set[A]) extends Set[A]:
 
-    infix def forAll(predicate: A => Boolean): Boolean = ???
+    infix def forAll(predicate: A => Boolean): Boolean =
+      left.forAll(predicate) && predicate(element) && right.forAll(predicate)
 
-    infix def exists(predicate: A => Boolean): Boolean = ???
+    infix def exists(predicate: A => Boolean): Boolean =
+      left.exists(predicate) || predicate(element) || right.exists(predicate)
 
-    infix def contains[B >: A: Order](x: B): Boolean = ???
+    infix def contains[B >: A: Order](x: B): Boolean =
+      if x == element then true
+      else if x < element then left.contains(x)
+      else right.contains(x)
 
-    infix def include[B >: A: Order](x: B): Set[B] = ???
+    infix def include[B >: A: Order](x: B): Set[B] =
+      if x == element then this
+      else if x < element then NonEmpty(left.include(x), element, right)
+      else NonEmpty(left, element, right.include(x))
 
     // Optional from the Unit 5. If you haven't implemented it in Unit 5 then skip it
     //    infix def remove[B >: A: Order](x: B): Set[B] = ???
 
     @targetName("union")
-    infix def ∪[B >: A: Order](that: Set[B]): Set[B] = ???
+    infix def ∪[B >: A: Order](that: Set[B]): Set[B] =
+      right ∪ (left ∪ that.include(element))
 
     @targetName("intersection")
-    infix def ∩[B >: A: Order](that: Set[B]): Set[B] = ???
+    infix def ∩[B >: A: Order](that: Set[B]): Set[B] =
+      val intersectionOfSubtrees = left ∩ that ∪ (right ∩ that)
+
+      if that.contains(element) then intersectionOfSubtrees.include(element)
+      else intersectionOfSubtrees
 
     // Optional from the Unit 5. If you haven't implemented it in Unit 5 then skip it
     //    @targetName("difference")
@@ -90,6 +103,10 @@ object set:
 
     override def toString: String = s"[$left - [$element] - $right]"
 
-    override def equals(obj: Any): Boolean = ???
+    override def equals(obj: Any): Boolean = obj match
+      case that: Set[?] =>
+        this.forAll(element => that.exists(_ == element)) &&
+        that.forAll(element => this.exists(_ == element))
+      case _ => false
 
   end NonEmpty
